8ebfc004e29c8fb6ff50e449ddbe6ac55785e48f
geethanjalig56 <g.gayan9451@gmail.com>
2024-02-09 16:41:13 +0530

    completed sign in

--- controller/userRoute.js
+++ controller/userRoute.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const userEntryModel = require("../model/userEntryModel")
 const bcrypt = require("bcryptjs")
+const { request } = require("http")
 
 hashPasswordGenerator = async(pswd)=>{
     const salt = await bcrypt.genSalt(10)
@@ -27,6 +28,24 @@ router.post("/signup", async(req,res)=>{
     )
     // let result = await userEntryObj.save()
    // res.json({status:"success save"})
+})
+
+router.post("/signin", async(req,res)=>{
+    let inputemail = req.body.email 
+    let data = await userEntryModel.findOne({"email": inputemail})
+    if(!data)
+    {
+      return res.json({status:"invalid email id"})
+    }
+    let dbPassword = data.password
+    let inputpassword = req.body.password
+    const match = await bcrypt.compare(inputpassword,dbPassword)
+    if(!match)
+    {
+        res.json({status:"incorrect password"})
+    }
+    res.json({status:"success"})
+
 })
 
 module.exports = router
